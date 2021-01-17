0051572583a146cb8f5329b629a158b303188f69
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-01-17 17:42:30 +0900

    learn useRef and useEffect in typescript

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const Counter: React.FC<{}> = () => {
-  const [value, setValue] = useState<number>(0)
+  const initialValue: any = 0
+  const [value, setValue] = useState<number>(initialValue)
 
   const increment = () => {
     setValue((prevState) => prevState + 1)
@@ -11,11 +12,17 @@ const Counter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1)
   }
 
+  const renderTimes = useRef<number>(0)
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1
+  })
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
     </div>
   )
 }
